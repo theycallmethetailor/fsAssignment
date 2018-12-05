12ac303b856b118e02e6f9358080bb54960983e0
Amanda <aeverett1317@gmail.com>
2018-12-05 14:16:27 -0700

    completed challenge 2

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ var fs = require('fs');
 
 //CHALLENGE 1
 console.log(fs.readFileSync('challenge1/info.txt', 'utf-8'));
+
+//CHALLENGE 2
+let info = fs.readFileSync('challenge2/info.txt', 'utf-8')
+
+info = 'Jun\nGreg\nEdwin\nRico\nCindy\nDavid\nClarance\nNancy\n' + info
+
+fs.writeFileSync('challenge2/info.txt', info)
+
+// fs.unlinkSync('info.txt')
